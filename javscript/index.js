69977e084aafd27cdad865c88be5492ea92d9169
Your Name <you@example.com>
2020-09-23 17:40:16 +0545

    changes

--- javscript/index.js
+++ javscript/index.js
@@ -1,20 +1,30 @@
+
+
+const loader = document.querySelector('.loading');
+
+const myLoader = () => {
+	loader.style.display = 'none';
+}
+
 const search = document.querySelector(".pink-search");
-const loc = document.querySelector(
-  "body > header > div > div.header > div.user-links > div.location"
-);
+const loc = document.querySelector("#location");
+const clickText = document.querySelector("#location .on-click-text");
 search.addEventListener("click", (e) => {
   const input = document.querySelector(".tab input");
   input.focus();
-  console.log(input);
 });
 
 loc.addEventListener("click", (e) => {
-  const clickText = document.querySelector(
-    "body > header > div > div.header > div.user-links > div.location > div.on-click-text"
-  );
   if (clickText.style.display === "none") {
     clickText.style.display = "block";
   } else {
+    clickText.style.display = "none";
+  }
+});
+
+window.addEventListener("click", (event) => {
+  const svg = document.querySelector("#location .globe svg path");
+  if (event.target !== loc) {
     clickText.style.display = "none";
   }
 });
